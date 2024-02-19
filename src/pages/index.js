22941c9b8351d9137ffa49665af93fbb61bc8077
Jefferson Pires <jeffersfp@gmail.com>
2024-02-19 15:22:50 -0300

    Adding a new message

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import * as React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <p>New message.</p>
+    </>
+  )
 }
